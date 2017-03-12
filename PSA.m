09ac85171b107c21323541d8240f7fa7c2711241
tc-imba <liuyh615@sjtu.edu.cn>
2017-03-12 16:18:37 +0800

    Fix some display position

--- PSA.m
+++ PSA.m
@@ -35,15 +35,35 @@ for i=1:2
     xlabel('year');
     flag=ones(n);
     num=1;
-    for j=1:n
-        for k=1:j-1
+    for j=n:-1:1
+        for k=n:-1:j+1
             if(abs(data(k,i+2)-data(j,i+2))<err(i)&&abs(time(k)-time(j))<300&&flag(k)==1)
                 flag(j)=0;
                 break;
             end
         end
-        if(flag(j))
-            text(time(j),data(j,i+2)-err(i)/2,[num2str(data(j,i+2)) ' (' timestr(j,:) ')']);
+        symbol=0;
+        if(j==1)
+            symbol=-3;
+        elseif(j==n)
+            symbol=-1;
+        elseif(data(j,i+2)>data(j+1,i+2)&&flag(j))
+            symbol=1;
+        elseif(flag(j))
+            symbol=-1;
+        else
+            for k=j+1:n
+                if(data(j,i+2)>data(k,i+2))
+                    if(abs(time(k)-time(j))>300||k==n)
+                        symbol=1;
+                    end
+                else break;
+                end
+            end
+        end
+        if(symbol~=0)    
+            line(time(j)+[0,50],data(j,i+2)+[0,symbol*err(i)/2],'LineStyle',':');
+            text(time(j)+50,data(j,i+2)+symbol*err(i)/2,[num2str(data(j,i+2)) ' (' timestr(j,:) ')']);
             num=num+1;
         end
     end
